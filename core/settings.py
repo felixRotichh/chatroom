6771615b3371e7a6c3b59a966b3d111f9dd52168
felixrotichh <felixkibet2000@gmail.com>
2025-01-03 19:20:54 +0300

    Added routing and consumer

--- core/settings.py
+++ core/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ASGI_APPLICATION = 'core.asgi.application'
+ASGI_APPLICATION = 'core.routing.application'

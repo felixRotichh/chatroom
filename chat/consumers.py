f8f2a1fe46f2212ff50285ddcfee66b21e3f5c57
felixrotichh <felixkibet2000@gmail.com>
2025-01-05 22:04:33 +0300

    finishing up on frontend

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,16 +3,20 @@ import json
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        # Extract room name from URL
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.room_group_name = f'chat_{self.room_name}'
 
+        # Add connection to the group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
+        # Accept the WebSocket connection
         await self.accept()
 
+        # Notify group about the new user
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -21,15 +25,45 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
             }
         )
 
+    async def receive(self, text_data):
+        # Handle incoming WebSocket messages
+        data = json.loads(text_data)
+        message = data['message']
+
+        # Broadcast the message to the group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
     async def chat_message(self, event):
+        # Send message to WebSocket
         message = event['message']
 
-        await self.send(text_data=json.dumps({
+        await self.send(text_data= json.dumps({
             'message': message
         }))
 
     async def disconnect(self, close_code):
+        # Remove connection from the group
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
+        )
+
+    async def receive(self, text_data):
+        # Handle incoming WebSocket messages
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # Broadcast the message to the group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
         )
